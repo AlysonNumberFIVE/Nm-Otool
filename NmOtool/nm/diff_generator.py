0300fe78d968bce90491ce931941e963e251f886
Alyson NGONYAMA <angonyam@e4r20p5.wethinkcode.co.za>
2018-07-20 14:27:06 +0200

    pushing nm agaoin

--- NmOtool/nm/diff_generator.py
+++ NmOtool/nm/diff_generator.py
@@ -1,14 +1,28 @@
 
 
+
+
+
 import sys
 import os
 
-def diff_generator():
+def diff_generator(filename):
 
-    filename = sys.argv[1]
+    print(' FILENAME >>> ', filename)
     arg = './ft_nm ' + filename + ' | sort > mine.txt'
     os.system(arg)
     arg = 'nm ' + filename + ' | sort > theirs.txt'
     os.system(arg)
+    os.system("diff mine.txt theirs.txt")
 
-diff_generator()
+def diff_loop():
+
+    pathway = sys.argv[1]
+    for root, dirs, files in os.walk(pathway):
+        path = root.split(os.sep)
+        print((len(path) - 1) * '---', os.path.basename(root))
+        for f in files:
+            st = pathway + '/' + f 
+            diff_generator(st)
+
+diff_loop()
